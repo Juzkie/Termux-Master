7cd10c29fc2ac3a58b04e16678b4af3ed1b422cb
Juzkie <47498822+Juzkie@users.noreply.github.com>
2020-06-14 17:18:01 +0800

    Create termux-master.sh

---
+++ termux-master.sh
@@ -0,0 +1,39 @@
+#Code-name: termux-master.sh
+#encoding: UTF8
+
+print"Installing..."
+
+apt update && upgrade
+apt install git -y
+apt install python -y
+apt install python2 -y
+apt install python3 -y
+apt install curl
+apt install php -y
+apt install ruby -
+apt install perl -y
+apt install pip -y
+apt install pip3 -y
+apt install wget -y
+apt install mtools -y
+apt install nano -y
+apt install ngrok -y
+apt install coreutils -y
+apt install termux-api -y
+apt install cmatrix -y
+apt install w3m -y
+apt install fish -y
+apt install toilet -y
+apt install golang -y
+apt install hydra -y
+apt install openssh -y
+apt install clang -y
+apt install figlet -y
+apt install termux-auth -y
+apt install nmap -y
+pkg install unstable-repo -y
+pkg install root-repo -y
+apt install tor -y
+apt install sl -y
+
+print"Installed √"
